d2da3aa08b6db054dc9bba1da436ab4302c2ca51
joonho319 <jejunho820@gmail.com>
2022-10-16 21:21:38 +0900

    S

--- pages/api/getLecture.ts
+++ pages/api/getLecture.ts
@@ -10,7 +10,7 @@ mail.setApiKey('SG.dJp25YCXTKWDYIEl9DLOVQ.aHzytzNJ1GKpq4vj0_wbhuEVpKxevG7Ws1ULlG
 async function handler (
   req: NextApiRequest, res: NextApiResponse<ResponseType>
 ) {
-  const count = await client.review.count();
+  const count = await client.lecture.count();
   const lecture = await  client.lecture.findMany({
     take: 15,
     skip: (parseInt(req.query.page.toString())-1)*15,
