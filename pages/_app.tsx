54bcafcfb172b591da8c06e09853d58feb457941
joonho319 <jejunho820@gmail.com>
2023-01-03 13:05:28 +0900

    prerender error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,15 +10,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <SWRConfig value={{fetcher: (url: string ) => fetch(url).then((response) => response.json())}}>
       <div className="w-full mx-auto">
         <Head>
-          <title>모두다 행정사</title>
-          <meta name="description" content="모두다 행정사"></meta>
-          <meta property="og:type" content="website"> 
-            <meta property="og:title" content="모두다 행정사" />
-            <meta property="og:description" content="모두다 행정사" />
-            <meta property="og:url" content="http://www.moduda-life.com"></meta>
-          </meta>
-          <meta name="naver-site-verification" content="566cff16301ad21a65297a04da6dcaa3c99379ca" />
-
         </Head>
         <Component {...pageProps} />
       </div>
