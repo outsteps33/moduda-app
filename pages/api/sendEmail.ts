1a07a64ec4e9f21e142a3979c69f4b5c5885a7ab
제준호 <junhoje@jejunhoui-noteubug.local>
2023-09-15 17:06:59 +0900

    v

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -4,6 +4,8 @@ import { withApiSession } from '@libs/server/withSession';
 import mail from '@sendgrid/mail';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
+import { SMTPClient } from 'emailjs';
+
 
 mail.setApiKey(process.env.SENDGRID_KEY!);
 
@@ -25,29 +27,8 @@ async function handler (
     });
     // 메일 옵션
   
-    if(type ==='상담신청') {
-      const mailOptions = {
-        from: "outsteps2023@gmail.com", // 보내는 메일의 주소
-        to: "modudahang@naver.com", // 수신할 이메일
-        html: 
-        `<h2><strong>[SMS 빠른 상담신청]</strong><h2>
-             1.이름: ${name} <br />
-             2. 번호: ${phone}<br />
-             3. 상담내용: ${comment}<br />
-        `,
-        subject : 'SMS 빠른 상담신청 ', // 메일 제목
-        // text: param.text, // 메일 내용
-        
-      };
+    if(type ==='진단신청') {
 
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-    } else if(type ==='진단신청') {
       const mailOptions = {
         from: "outsteps2023@gmail.com", // 보내는 메일의 주소
         to: "modudahang@naver.com", // 수신할 이메일
@@ -88,11 +69,33 @@ async function handler (
         }
       });
 
-      setTimeout(() => {
-        
-      }, 3000);
 
     }
+    
+    if(type ==='상담신청') {
+      const mailOptions = {
+        from: "outsteps2023@gmail.com", // 보내는 메일의 주소
+        to: "modudahang@naver.com", // 수신할 이메일
+        html: 
+        `<h2><strong>[SMS 빠른 상담신청]</strong><h2>
+             1.이름: ${name} <br />
+             2. 번호: ${phone}<br />
+             3. 상담내용: ${comment}<br />
+        `,
+        subject : 'SMS 빠른 상담신청 ', // 메일 제목
+        // text: param.text, // 메일 내용
+        
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    } 
+    
     
     
     // 메일 발송 
