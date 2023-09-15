5c62797c17f7a8ca54bfb41e53fde33eea10bd72
제준호 <junhoje@jejunhoui-noteubug.local>
2023-09-16 00:08:44 +0900

    v

--- pages/api/sendEmail2.ts
+++ pages/api/sendEmail2.ts
@@ -18,13 +18,13 @@ async function handler (
  
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      host: 'imap.gmail.com',
-      port: 993,
+      host: 'smtp.gmail.com',
+      port: 587,
       secure: false,
       //secure가 false라고 해서 암호화된 연결을 사용하지 않는다는 의미가 아닙니다.
       auth: {
-        user: "outsteps2023@gmail.com",
-        pass: "zklzecllduxapkvx",
+        user: "jejunho820@gmail.com",
+        pass: "kvrfqnvaydubskbm",
       },
     });
     // 메일 옵션
@@ -48,7 +48,7 @@ async function handler (
     
       console.log(t)
       const mailOptions = {
-        from: "outsteps2023@gmail.com", // 보내는 메일의 주소
+        from: "jejunho820@gmail.com", // 보내는 메일의 주소
         to: "modudahang@naver.com", // 수신할 이메일
        
         subject : '구제 성공 가능성 진단', // 메일 제목
