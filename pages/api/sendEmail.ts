17c187e8b52042dc9bd2c836265c47c10ba0b9ae
제준호 <junhoje@jejunhoui-noteubug.local>
2023-09-15 16:09:07 +0900

    s

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -75,14 +75,20 @@ async function handler (
     }
     
     
-    // 메일 발송    
-     transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+    // 메일 발송 
+    
+    const sendMail = async() => {
+      await transporter.sendMail(mailOptions, function (error, info) {
+       if (error) {
+         console.log(error);
+       } else {
+         console.log('Email sent: ' + info.response);
+       }
+     });
+    }
+    Promise.all([
+      sendMail()
+    ])
   } catch(e) {
     console.log(e);
   }
