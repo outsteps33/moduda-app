976c6f436a4c89248e4e27438a11f82c42f514ea
제준호 <junhoje@jejunhoui-noteubug.local>
2023-09-15 16:57:20 +0900

    v

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -87,6 +87,15 @@ async function handler (
           console.log('Email sent: ' + info.response);
         }
       });
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
     }
     
     
