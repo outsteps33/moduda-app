eb852e9d607cd980e189b2b4a20ac405a4344a9d
RivuletJo <162682814+RivuletJo@users.noreply.github.com>
2024-06-28 14:00:54 +0900

    Update Footer TEST6.tsx
    기존 포스터로 롤백
    사유: 버셀 배포 문제 해결용

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,29 +37,27 @@ const FooterDescription = styled.div`
 
 export const Footer = () => {
   const history = useRouter();
-
-    console.log('Footer component loaded');
-  
   return (
-    <FooterContainer className="border-t-[1px]">
+    <FooterContainer className="border-t-[1px] ">
       <FooterDescriptions>
         <FooterDescription>
           <div className="sm:pt-1 hidden sm:grid sm:justify-center mb-[3px]">
-            <Image src={Logo} width={200} height={44.12} layout="fixed" alt="Logo" />
+            <Image src={Logo}  width={200} height={44.12} layout="fixed"/>
           </div>
-          <div className="sm:hidden grid justify-center">
-            <Image src={Logo} width={151} height={34.09} layout="fixed" alt="Logo" />
+          <div className=" sm:hidden grid justify-center ">
+            <Image src={Logo}  width={151} height={34.09} layout="fixed"/>
           </div>
-          <div className="text-[12px] sm:text-[15px] text-center mt-[2px] text--gray-500">
+          <div className="text-[12px] sm:text-[15px] text-center mt-[2px] text-gray-500">
+
             사업자등록번호 : 418-08-53233 <br /> 대표자 : 송범석 | 개인정보보호책임자 : 송범석 <br />
             대표번호 : 010-8662-5465 ( AM 08:00 - PM 11:00 )<br />
-            전북특별자치도 전주시 덕진구 만성로 128 1동 4층 405-B호<br />
-            © Copyright 2022 모두다행정사 All Right Reserved <br />
+              © 2022 CCS, Inc. All rights reserved.<br />
+              Copyright 2022 모두다행정사 All Right Reserved <br />
             모두다행정사는 ‘전자상거래 등의에서의 소비자보호에 관한 법률’에 따라 통신판매업 신고 등록을 마친 전문가 입니다.<br />
-            서비스이용약관 | 개인정보취급방침 | 이메일무단수집거부<br />
+              서비스이용약관 | 개인정보취급방침 | 이메일무단수집거부<br />
           </div>
         </FooterDescription>
       </FooterDescriptions>
     </FooterContainer>
-  );
-};
+  )
+}
