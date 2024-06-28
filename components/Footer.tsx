b45c2268950bcd2925857b685420d25af9b85caa
RivuletJo <162682814+RivuletJo@users.noreply.github.com>
2024-06-28 12:18:43 +0900

    Update Footer TEST3.tsx
    전체 코드 수정(일부 구간만 수정 후 전체 덧씌우기)
    확인 후 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,24 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import Logo from '../images/black_logo.png';
 import Image from 'next/image';
 
 const FooterContainer = styled.div`
   width: 100vw;
-  padding-top:10px;
-  margin-bottm: 200px;
+  padding-top: 10px;
+  margin-bottom: 200px;
   @media (max-width: 768px) { 
-    padding-bottm: 10px;
+    padding-bottom: 10px;
   }
   bottom: 0;
-  // background: #2D2E42; 
-
-
 `;
 
 const FooterDescriptions = styled.div`
   display: grid;
   place-content: center;
   height: 100%;
-  // padding-top: 50px;
- 
   padding-bottom: 10px;
 `;
 
@@ -37,34 +32,31 @@ const FooterDescription = styled.div`
     font-size: 16px;
     line-height: 25px;
     color: #8A8A8A;
-  
   }  
 `;
 
 export const Footer = () => {
   const history = useRouter();
   return (
-    <FooterContainer className="border-t-[1px] ">
+    <FooterContainer className="border-t-[1px]">
       <FooterDescriptions>
         <FooterDescription>
           <div className="sm:pt-1 hidden sm:grid sm:justify-center mb-[3px]">
-            <Image src={Logo}  width={200} height={44.12} layout="fixed"/>
+            <Image src={Logo} width={200} height={44.12} layout="fixed" alt="Logo" />
           </div>
-          <div className=" sm:hidden grid justify-center ">
-            <Image src={Logo}  width={151} height={34.09} layout="fixed"/>
+          <div className="sm:hidden grid justify-center">
+            <Image src={Logo} width={151} height={34.09} layout="fixed" alt="Logo" />
           </div>
-          <div className="text-[12px] sm:text-[15px] text-center mt-[2px] text-gray-500">
-
+          <div className="text-[12px] sm:text-[15px] text-center mt-[2px] text--gray-500">
             사업자등록번호 : 418-08-53233 <br /> 대표자 : 송범석 | 개인정보보호책임자 : 송범석 <br />
             대표번호 : 010-8662-5465 ( AM 08:00 - PM 11:00 )<br />
-            주소 확인<br />
-              © Copyright 2022 모두다행정사 All Right Reserved <br />
+            전북특별자치도 전주시 덕진구 만성로 128 1동 4층 405-B호<br />
+            © Copyright 2022 모두다행정사 All Right Reserved <br />
             모두다행정사는 ‘전자상거래 등의에서의 소비자보호에 관한 법률’에 따라 통신판매업 신고 등록을 마친 전문가 입니다.<br />
-              서비스이용약관 | 개인정보취급방침 | 이메일무단수집거부<br />
+            서비스이용약관 | 개인정보취급방침 | 이메일무단수집거부<br />
           </div>
         </FooterDescription>
       </FooterDescriptions>
     </FooterContainer>
-  )
-}
-
+  );
+};
