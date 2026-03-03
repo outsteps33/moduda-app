64ae8a62bf6f3e4cabcd2dae51279eead73359fc
outsteps33 <124334484+outsteps33@users.noreply.github.com>
2026-03-03 15:40:40 +0900

    Update agreement.tsx

--- pages/agreement.tsx
+++ pages/agreement.tsx
@@ -20,13 +20,3 @@ const Agreement: NextPage = () => {
 
 export default Agreement;
 
-<script type="text/javascript" src="//wcs.naver.net/wcslog.js"> </script> 
-<script type="text/javascript"> 
-if (!wcs_add) var wcs_add={};
-wcs_add["wa"] = "s_22b8d0fae23f";
-if (!_nasa) var _nasa={};
-if(window.wcs){
-wcs.inflow();
-wcs_do();
-}
-</script>
